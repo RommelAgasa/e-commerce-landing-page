bba433140dbb538faf76aacdf31ac90b53087d6d
_DevNote <rommelagasa102@gmail.com>
2025-10-01 20:33:22 +0800

    done making the hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,21 +1,26 @@
-
+import headphone from '../assets/img/headphone.png'
 
 export default function Hero(){
 
-
     return (
-        <div className="bg-zinc-200 flex justify-center items-center py-32 p-2 rounded-2xl">
-            <div>
+        <div className="bg-zinc-200 flex justify-center items-center py-28 p-2 rounded-2xl relative">
+            <div className="leading-none px-20">
                 <div className="font-medium text-4xl">Beats Solo</div>
-                <div className="font-semibold text-7xl" >Wireless</div>
-                <div className="font-bold p-0 m-0 text-[250px] text-white uppercase border">Headphone</div>
-                <button>
+                <div className="font-semibold text-[100px]">Wireless</div>
+                <div className="font-bold text-[200px] leading-[1] text-white uppercase">
+                    Headphone
+                </div>
+                <button className="bg-red-600 hover:bg-red-700 p-4 rounded-3xl text-white cursor-pointer transition-colors">
                     Shop by Category
                 </button>
-            </div>
-            <div>
 
             </div>
+
+            {/* Headphone Image */}
+            <div className="absolute inset-0 flex justify-center items-center pointer-events-none">
+                {headphone && <img src={headphone} alt="headphone" className="max-h-[80%]" />}
+            </div>
         </div>
+
     );
 }
